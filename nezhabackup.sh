72aaa6b18c03a12344dbd41cd0227a9f6e5a5870
CodionX <60713286+codionx@users.noreply.github.com>
2021-07-08 01:53:11 +0800

    Update nezhabackup.sh

--- nezhabackup.sh
+++ nezhabackup.sh
@@ -2,5 +2,4 @@
 cd /opt
 rm -rf nezha.tgz
 tar -zcvf nezha.tgz /opt/nezha
-mkdir ~/wow/cc-backup/data/nezha/"$(date +"%Y-%m-%d")"
-cp -rf /opt/nezha.tgz ~/wow/cc-backup/data/nezha/"$(date +"%Y-%m-%d")"
+cp -rf /opt/nezha.tgz ~/wow/cc-backup/data/nezha/nezha_"$(date +"%Y-%m-%d_%H-%M")".tgz
