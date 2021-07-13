995ebbea7ac1977d386bb0b35c7d0ff51c10dbe6
CodionX <60713286+codionx@users.noreply.github.com>
2021-07-14 03:20:26 +0800

    Update backup_portainer_nocn.sh

--- backup_portainer_nocn.sh
+++ backup_portainer_nocn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf po_data.tgz
-tar -zcvf po_data.tgz /data/portainer/data
+tar -zcvf ~/po_data.tgz /data/portainer/data
 cp -rf ~/po_data.tgz ~/wow/cc-backup/data/portainer/po_data_"$(date +"%Y-%m-%d_%H-%M")".tgz
