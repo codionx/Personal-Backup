cf825086eba1c56fe578a9d6b79206c2eba1f29b
CodionX <60713286+codionx@users.noreply.github.com>
2021-07-14 20:09:47 +0800

    Update backup_portgo_cn.sh

--- backup_portgo_cn.sh
+++ backup_portgo_cn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/portgo
 rm -rf data.sql
-docker-compose exec postgres pg_dump -d aurora -U aurora -c > data.sql
+docker-compose exec -T postgres pg_dump -d aurora -U aurora -c > data.sql
 cp -rf ~/portgo/data.sql ~/wow/tera/data/portgo/data_"$(date +"%Y-%m-%d_%H-%M")".sql
